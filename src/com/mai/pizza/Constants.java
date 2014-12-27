faee77c97b787ea2474317d23eb867e0f786ecba
L1S <itsnotl1s@ya.ru>
2014-12-27 21:34:40 +0300

    fixed all warnings

--- src/com/mai/pizza/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.mai.pizza;
-
-public class Constants {
-    public static int ORDER_STATUS_ACTIVE = 0;
-}
---
+++ src/main/java/com/mai/util/SessionUtil.java
@@ -0,0 +1,28 @@
+package com.mai.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * Created by L1S on 27.12.2014.
+ */
+public class SessionUtil {
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    private static SessionFactory configureSessionFactory() throws HibernateException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return configureSessionFactory();
+
+    }
+}
